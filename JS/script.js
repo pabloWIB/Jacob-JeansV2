6c186920632155051cf421614dc8c832baeffe6b
pabloWIB <pablonietoperez369@gmail.com>
2024-12-26 22:48:35 -0500

    Cambios

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,40 @@
-$(function(){
-    setTimeout(function(){
-        $("#video").addClass("animationVideo1");
-    }, 700);
+$(function () {
+	setTimeout(function () {
+		$("#video").addClass("animationVideo1");
+	}, 700);
 });
 
-$(function(){
-    setTimeout(function(){
-        $("#headerContent").addClass("navBarAnimation");
+$(function () {
+	setTimeout(function () {
+		$("#headerContent").addClass("navBarAnimation");
 		$(".mainContent").addClass("mainContentrAnimation");
-    }, 2500);
+	}, 2500);
 });
 
-$(function(){
-    setTimeout(function(){
+$(function () {
+	setTimeout(function () {
 		$(".footerContent").addClass("mainContentrAnimation");
-    }, 2700);
+	}, 2700);
 });
 
 
-$(function(){
-	$(".Menu").click(function(e){
+$(function () {
+	$(".Menu").click(function (e) {
 		$(".overlayMenu").toggleClass("overlayMenuOpen");
 		$(".backVid").toggleClass("animationVideo2");
 		$("body").toggleClass("bodyOverflowAnimation");
 	});
 });
 
-$(function(){
-	$(".sectionBackground").hover(function(e){
+$(function () {
+	$(".sectionBackground").hover(function (e) {
 		$(".sectionBackground").toggleClass("backgroundAnimation");
 	});
 });
 
-$(function(){
-	$(".findArrowUnderline").hover(function(e){
-		$(".textArea2").toggleClass("pene");
+$(function () {
+	$(".findArrowUnderline").hover(function (e) {
+		$(".textArea2").toggleClass("arroyLineAnimation");
 	});
 });
 
